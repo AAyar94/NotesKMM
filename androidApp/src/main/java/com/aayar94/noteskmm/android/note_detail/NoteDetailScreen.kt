28b20afac92292fb93d5dbd5a776a40f22148ac5
Adem Ayar <79112917+AAyar94@users.noreply.github.com>
2023-08-24 18:45:29 +0300

    Note detail screen created

---
+++ androidApp/src/main/java/com/aayar94/noteskmm/android/note_detail/NoteDetailScreen.kt
@@ -0,0 +1,87 @@
+package com.aayar94.noteskmm.android.note_detail
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+
+@Composable
+fun NoteDetailScreen(
+    noteId: Long,
+    navController:NavController,
+    viewModel: NoteDetailViewModel = hiltViewModel()
+) {
+
+    val state by viewModel.state.collectAsState()
+    val hasNoteBeenSaved by viewModel.hasNoteBeenSaved.collectAsState()
+
+    LaunchedEffect(key1 = hasNoteBeenSaved, block = {
+        if (hasNoteBeenSaved) {
+            navController.popBackStack()
+        }
+    })
+
+    Scaffold(floatingActionButton = {
+        FloatingActionButton(
+            onClick = { viewModel.saveNote() }, backgroundColor = Color.Black
+        ) {
+            Icon(
+                imageVector = Icons.Default.Check,
+                contentDescription = "Save Note",
+                tint = Color.White
+            )
+        }
+    }) { padding ->
+        Column(
+            modifier = Modifier
+                .background(Color(state.noteColor))
+                .fillMaxSize()
+                .padding(padding)
+                .padding(16.dp)
+        ) {
+            TransparentHintTextField(
+                text = state.noteTitle,
+                hint = "Enter a title....",
+                isHintVisible = state.isNoteTitleHintVisible,
+                onValueChanged = viewModel::onNoteTitleChanged,
+                onFocusChanged = {
+                    viewModel.onNoteTitleFocusChanged(it.isFocused)
+                },
+                singleLine = true,
+                textStyle = TextStyle(fontSize = 20.sp)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            TransparentHintTextField(
+                text = state.noteContent,
+                hint = "Enter some content....",
+                isHintVisible = state.isNoteContentHintVisible,
+                onValueChanged = viewModel::onNoteContentChanged,
+                onFocusChanged = {
+                    viewModel.onNoteContentFocusChanged(it.isFocused)
+                },
+                singleLine = false,
+                textStyle = TextStyle(fontSize = 20.sp),
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
+}
---
+++ androidApp/src/main/java/com/aayar94/noteskmm/android/note_detail/NoteDetailState.kt
@@ -0,0 +1,9 @@
+package com.aayar94.noteskmm.android.note_detail
+
+data class NoteDetailState(
+    val noteTitle: String = "",
+    val isNoteTitleHintVisible: Boolean = false,
+    val noteContent: String = "",
+    val isNoteContentHintVisible: Boolean = false,
+    val noteColor: Long = 0xFFFFFFFF,
+)
