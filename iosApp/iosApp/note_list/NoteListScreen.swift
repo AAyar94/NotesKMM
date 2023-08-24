3bd6e190e80907da5ee4e87a31a321b6a21e936c
Adem Ayar <79112917+AAyar94@users.noreply.github.com>
2023-08-24 23:27:58 +0300

    IOS app List screen inited, IOS app list screen viewmodel created

---
+++ iosApp/iosApp/note_list/NoteListScreen.swift
@@ -0,0 +1,21 @@
+//
+//  NoteListScreen.swift
+//  iosApp
+//
+//  Created by Adem Ayar on 24.08.2023.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct NoteListScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NoteListScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        NoteListScreen()
+    }
+}
